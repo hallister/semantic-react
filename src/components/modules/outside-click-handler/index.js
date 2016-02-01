5a622dc1f441f548385aec12a3a3a10389e80ea8
Popov Anatoly <a.popov@office.ngs.ru>
2016-02-01 17:32:22 +0600

    add outside click handler component

---
+++ src/components/modules/outside-click-handler/index.js
@@ -0,0 +1,38 @@
+/**
+ * Outside click document
+ */
+
+import React from 'react';
+
+export default class OutsideClickHandler extends React.Component {
+    static propTypes = {
+        onOutsideClick: React.PropTypes.func
+    };
+
+    componentDidMount() {
+        document.addEventListener('click', this.handleDocumentClick, false);
+    }
+    
+    componentWillUnmount() {
+        document.removeEventListener('click', this.handleDocumentClick, false);
+    }
+
+    handleDocumentClick(event) {
+        if (this.props.onOutsideClick !== null) {
+            return this.props.onOutsideClick(event);
+        }
+    }
+
+    handleMyClick(event) {
+        event.stopPropagation();
+        event.nativeEvent.stopImmediatePropagation();
+    }
+
+    render() {
+        return (
+            <div onClick={this.handleMyClick.bind(this)}>
+                {this.props.children}
+            </div>
+        );
+    }
+}
