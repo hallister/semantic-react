e4b179488321c4943c83be6bd9080e52911d433b
Alexey Svetliakov <asvetliakov@teledini.com>
2016-02-05 00:30:46 +0100

    es 6 exports.

--- dist/semantic-react.js
+++ dist/semantic-react.js
@@ -56,6 +56,14 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	'use strict';
 
+	Object.defineProperty(exports, '__esModule', {
+	  value: true
+	});
+
+	function _interopExportWildcard(obj, defaults) { var newObj = defaults({}, obj); delete newObj['default']; return newObj; }
+
+	function _defaults(obj, defaults) { var keys = Object.getOwnPropertyNames(defaults); for (var i = 0; i < keys.length; i++) { var key = keys[i]; var value = Object.getOwnPropertyDescriptor(defaults, key); if (value && value.configurable && obj[key] === undefined) { Object.defineProperty(obj, key, value); } } return obj; }
+
 	function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj['default'] = obj; return newObj; } }
 
 	var _elements = __webpack_require__(1);
@@ -83,6 +91,16 @@ return /******/ (function(modules) { // webpackBootstrap
 	exports.utilites = utilites;
 	exports.views = views;
 	exports.collections = collections;
+
+	_defaults(exports, _interopExportWildcard(_elements, _defaults));
+
+	_defaults(exports, _interopExportWildcard(_modules, _defaults));
+
+	_defaults(exports, _interopExportWildcard(_utilities, _defaults));
+
+	_defaults(exports, _interopExportWildcard(_views, _defaults));
+
+	_defaults(exports, _interopExportWildcard(_collections, _defaults));
 
 /***/ },
 /* 1 */
