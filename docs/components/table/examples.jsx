81a606a683dc092b591a08272600fa23a7a7b6b8
aphofstede <alex@hofste.de>
2016-02-22 02:15:39 +0700

    Added padded table example

--- docs/components/table/examples.jsx
+++ docs/components/table/examples.jsx
@@ -1,7 +1,8 @@
 import { Colors } from '../../utilities';
 import { Table, Tr, Td }from '../../../src/components/collections';
-import { Label, Icon }from '../../../src/components/elements';
+import { Label, Icon, Header }from '../../../src/components/elements';
 import { Menu, Item }from '../../../src/components/views';
+import { Rating }from '../../../src/components/modules';
 
 export default {
     Celled: {
@@ -173,6 +174,122 @@ export default {
                             }
                         }
                 }
+
+            }
+        ]
+    },
+    Padded: {
+        component: Table,
+        props: { celled: true, padded: true },
+        children: [
+            {
+                component: 'thead',
+                children:
+                    {
+                        component: Tr,
+                        children: [
+                            {
+                                component: 'th',
+                                props: {
+                                    className: 'single line' // TODO Time to create a Th component?
+                                },
+                                children: 'Evidence Rating'
+                            },
+                            { component: 'th', children: 'Effect' },
+                            { component: 'th', children: 'Efficacy' },
+                            { component: 'th', children: 'Consensus' },
+                            { component: 'th', children: 'Comments' }
+                        ]
+                    }
+
+            },
+            {
+                component: 'tbody',
+                children: [
+                    {
+                        component: Tr,
+                        children: [
+                            {
+                                component: Td,
+                                children: {
+                                    component: Header,
+                                    props: { aligned: 'center' },
+                                    children: 'A'
+                                }
+                            },
+                            {
+                                component: Td,
+                                props: { singleLine: true },
+                                children: 'Power Output'
+                            },
+                            {
+                                component: Td,
+                                children: {
+                                    component: Rating,
+                                    props: {
+                                        star: true,
+                                        max: 3 // FIXME Cannot set active through props
+                                    }
+                                }
+                            },
+                            {
+                                component: Td,
+                                props: { aligned: 'right' },
+                                children: [
+                                    '80%',
+                                    { component: 'br' },
+                                    { component: 'a', props: { href: '#' }, children: '18 studies' }
+                                ]
+                            },
+                            {
+                                component: Td,
+                                children: 'Creatine supplementation is the reference compound for increasing muscular creatine levels; there is variability in this increase, however, with some nonresponders.'
+                            }
+                        ]
+                    },
+                    {
+                        component: Tr,
+                        children: [
+                            {
+                                component: Td,
+                                children: {
+                                    component: Header,
+                                    props: { aligned: 'center' },
+                                    children: 'A'
+                                }
+                            },
+                            {
+                                component: Td,
+                                props: { singleLine: true },
+                                children: 'Weight'
+                            },
+                            {
+                                component: Td,
+                                children: {
+                                    component: Rating,
+                                    props: {
+                                        star: true,
+                                        max: 3 // FIXME Cannot set active through props
+                                    }
+                                }
+                            },
+                            {
+                                component: Td,
+                                props: { aligned: 'right' },
+                                children: [
+                                    '100%',
+                                    { component: 'br' },
+                                    { component: 'a', props: { href: '#' }, children: '65 studies' }
+                                ]
+                            },
+                            {
+                                component: Td,
+                                children: 'Creatine is the reference compound for power improvement, with numbers from one meta-analysis to assess potency'
+                            }
+                        ]
+                    }
+                ]
+
 
             }
         ]
