7c11c4626cb79692011205c53a17191ae6069cb8
Alexey Svetliakov <asvetliakov@teledini.com>
2016-03-09 03:07:28 +0100

    Added typescript typing definition Reworked menu to not use internal state, control via props now. Menu could contain few active items now

--- src/components/views/menu/__tests__/menu-test.jsx
+++ src/components/views/menu/__tests__/menu-test.jsx
@@ -6,7 +6,7 @@ import { shallow } from 'enzyme';
 import Menu from '../menu';
 import MenuItem from '../menuitem';
 
-describe.only('Menu', () => {
+describe('Menu', () => {
     it('It renders as div by default', () => {
         let wrapper = shallow(<Menu />);
         expect(wrapper).to.have.tagName('div');
