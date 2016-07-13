4052c3c04eff96d5ad72f65a6182d9b5deb44f5b
Alexey Svetliakov <asvetliakov@teledini.com>
2016-07-13 20:21:14 +0200

    Loader size prop type is more precise

--- src/components/elements/loader/loader.jsx
+++ src/components/elements/loader/loader.jsx
@@ -22,7 +22,7 @@ export default class Loader extends React.Component {
         /**
          * Loaders can have different sizes
          */
-        size: React.PropTypes.string,
+        size: React.PropTypes.oneOf(['mini', 'tiny', 'small', 'medium', 'large', 'big', 'huge', 'massive']),
         /**
          * Loader state
          */
